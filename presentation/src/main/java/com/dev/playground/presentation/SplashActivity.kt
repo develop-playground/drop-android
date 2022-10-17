39bf150a1b9a1b1df2bca3b7f0e6f0e8a07dcafb
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2022-10-17 17:15:31 +0900

    slpash view 껍데기 추가 및 코루틴 의존성 추가

---
+++ presentation/src/main/java/com/dev/playground/presentation/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.dev.playground.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+    }
+
+}
