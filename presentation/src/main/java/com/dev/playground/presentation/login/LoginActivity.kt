46674b3cdfc3a946b28f816e1eab8e97f19709f5
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2022-10-28 18:59:24 +0900

    kakao login button 누르면 웹 혹은 앱으로 로그인할 수 있는 로직 구현

--- presentation/src/main/java/com/dev/playground/presentation/login/LoginActivity.kt
+++ presentation/src/main/java/com/dev/playground/presentation/login/LoginActivity.kt
@@ -4,19 +4,42 @@ import android.os.Bundle
 import com.dev.playground.presentation.R
 import com.dev.playground.presentation.base.BaseActivity
 import com.dev.playground.presentation.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity: BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 
+    private val loginCallback: (OAuthToken?, Throwable?) -> Unit = { token, exception ->
+        if (exception != null) {
+            println("로그인 성공 $token")
+        } else {
+            println("로그인 실패")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initKakaoSdk()
+
+        binding.btLogin.setOnClickListener {
+            isKakaoLogin()
+        }
+
     }
 
     private fun initKakaoSdk() {
         KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+    }
+
+    private fun isKakaoLogin() {
+
+        when (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            true -> UserApiClient.instance.loginWithKakaoTalk(this, callback = loginCallback)
+            false -> UserApiClient.instance.loginWithKakaoAccount(this, callback = loginCallback)
+        }
+
     }
 
 }
