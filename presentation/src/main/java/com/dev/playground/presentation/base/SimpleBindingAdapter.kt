9b4d005130a11e8005afcd676d2c4e998ce30be1
hongbeomi <dks3963@gmail.com>
2022-11-06 22:46:51 +0900

    Simple Adapter, ViewHolder 선언

---
+++ presentation/src/main/java/com/dev/playground/presentation/base/SimpleBindingAdapter.kt
@@ -0,0 +1,33 @@
+package com.dev.playground.presentation.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.ListAdapter
+import com.dev.playground.presentation.model.base.SimpleItemDiffCallback
+import com.dev.playground.presentation.model.base.SimpleUIModel
+
+open class SimpleBindingAdapter<T, VH>(
+    private val viewHolderClazz: Class<VH>? = null,
+) : ListAdapter<T, VH>(
+    SimpleItemDiffCallback()
+) where T : SimpleItemDiffCallback.DiffCallback, T : SimpleUIModel, VH : SimpleBindingViewHolder<T> {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(
+            layoutInflater,
+            viewType,
+            parent,
+            false
+        )
+        return (viewHolderClazz?.getDeclaredConstructor(ViewDataBinding::class.java)?.newInstance(binding) ?: SimpleBindingViewHolder(binding)) as VH
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    override fun getItemViewType(position: Int): Int = getItem(position).layoutRes
+}
