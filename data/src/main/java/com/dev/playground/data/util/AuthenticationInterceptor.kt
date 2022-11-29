bc380a2bd2e95d2206e9eef3ca3a05d2aabc16a3
hongbeomi <dks3963@naver.com>
2022-11-30 01:25:16 +0900

    인터셉터는 OkHttp 내부에서 동작하게 되기 때문에, IOException을 던지지 않으면 OkHttp 바깥으로 에러를 전달하지 않아서 외부의 try-catch를 타지 않음.

--- data/src/main/java/com/dev/playground/data/util/AuthenticationInterceptor.kt
+++ data/src/main/java/com/dev/playground/data/util/AuthenticationInterceptor.kt
@@ -2,6 +2,7 @@ package com.dev.playground.data.util
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.io.IOException
 
 class AuthenticationInterceptor(
     private val preferencesManager: SharedPreferencesManager
@@ -13,9 +14,7 @@ class AuthenticationInterceptor(
     }
 
     override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
-
-        val accessToken =
-            preferencesManager.getToken()?.accessToken ?: throw IllegalArgumentException()
+        val accessToken = preferencesManager.getToken()?.accessToken ?: throw IOException()
 
         val newRequest = request().newBuilder()
             .addHeader(KEY_AUTH, "$TYPE_AUTH $accessToken")
