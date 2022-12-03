9a7a970d5bef86f25f70523f7eb68906b94f8b64
hongbeomi <dks3963@gmail.com>
2022-12-03 15:53:42 +0900

    상수 사용

--- presentation/src/main/java/com/dev/playground/presentation/ui/feed/FeedViewModel.kt
+++ presentation/src/main/java/com/dev/playground/presentation/ui/feed/FeedViewModel.kt
@@ -1,20 +1,21 @@
 package com.dev.playground.presentation.ui.feed
 
 import androidx.lifecycle.viewModelScope
-import com.dev.playground.domain.model.Memory
 import com.dev.playground.domain.usecase.memory.GetMemoryListUseCase
 import com.dev.playground.presentation.base.BaseViewModel
-import com.dev.playground.presentation.model.ImageCarouselItemUIModel
 import com.dev.playground.presentation.model.MemoryUIModel
 import com.dev.playground.presentation.model.toPresentation
 import com.dev.playground.presentation.ui.feed.FeedViewModel.FeedEvent
 import com.dev.playground.presentation.ui.feed.FeedViewModel.FeedState
 import kotlinx.coroutines.launch
 
-// TODO 로컬 DB or 네트워크에서 실제 데이터 받아와야 함.
 class FeedViewModel(
     private val getMemoryListUseCase: GetMemoryListUseCase,
 ) : BaseViewModel<FeedState, FeedEvent>(FeedState.Loading) {
+
+    companion object {
+        private const val EMPTY_SIZE = "0"
+    }
 
     init {
         fetch()
@@ -62,7 +63,7 @@ class FeedViewModel(
             get() = if (this is Success && this.itemList.isNotEmpty()) {
                 itemList.size.toString()
             } else {
-                "0"
+                EMPTY_SIZE
             }
     }
 
