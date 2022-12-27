48db0acb489749bc02c4b8f4774305432204235a
hongbeomi <dks3963@gmail.com>
2022-12-27 12:10:27 +0900

    액세스 토큰 만료 시 재발급 및 로그아웃 익셉션 처리

--- data/src/main/java/com/dev/playground/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/com/dev/playground/data/repository/AuthRepositoryImpl.kt
@@ -3,17 +3,23 @@ package com.dev.playground.data.repository
 import com.dev.playground.data.data_source.remote.AuthService
 import com.dev.playground.data.model.MemberType
 import com.dev.playground.data.model.toDomain
+import com.dev.playground.data.util.AutoTokenRefresher
 import com.dev.playground.domain.model.User
 import com.dev.playground.domain.repository.AuthRepository
 
-class AuthRepositoryImpl(private val service: AuthService) : AuthRepository {
+class AuthRepositoryImpl(
+    private val service: AuthService,
+    private val refresher: AutoTokenRefresher,
+) : AuthRepository {
 
     override suspend fun requestLogin(memberType: String) = service.requestLogin(MemberType(memberType)).toDomain()
 
-    override suspend fun getUser(): User = service.getUser().toDomain()
+    override suspend fun getUser(): User = refresher.authHandle { service.getUser().toDomain() }
 
-    override suspend fun deleteUser() = service.deleteUser()
+    override suspend fun deleteUser() = refresher.authHandle { service.deleteUser() }
 
-    override suspend fun postLogout() = service.postLogout()
+    override suspend fun postLogout() = refresher.authHandle { service.postLogout() }
+
+    override suspend fun refreshAccessToken(): String = service.refreshAccessToken().accessToken
 
 }
