f8d89ea0e368b599fd3116645bb9792b66945a26
hongbeomi <dks3963@gmail.com>
2023-01-04 21:57:28 +0900

    callbackFlow로 로띠 애니메이션 waiting, zip 함수 사용하여 애니메이션, Token 체크 완료되었을 때 Navigation 이벤트 실행

--- presentation/src/main/java/com/dev/playground/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/dev/playground/presentation/ui/splash/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.dev.playground.presentation.ui.splash
 
+import android.animation.Animator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dev.playground.presentation.R
@@ -14,33 +15,51 @@ import com.dev.playground.presentation.util.showToast
 import com.dev.playground.presentation.util.startActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     private val viewModel by viewModel<SplashViewModel>()
+    private val animationFlow by lazy {
+        callbackFlow {
+            val listener = object : Animator.AnimatorListener {
+                override fun onAnimationEnd(animation: Animator?) {
+                    binding.lottieSplash.progress = 0f
+                    trySend(Unit)
+                }
+                override fun onAnimationCancel(animation: Animator?) { }
+                override fun onAnimationRepeat(animation: Animator?) { }
+                override fun onAnimationStart(animation: Animator?) { }
+            }
+            binding.lottieSplash.addAnimatorListener(listener)
+
+            awaitClose {
+                binding.lottieSplash.removeAllAnimatorListeners()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            delay(2450L)
-        }
         initCollects()
     }
 
     private fun initCollects() = with(viewModel) {
         repeatOnLifecycleState {
             launch {
-                effect.collect {
-                    when (it) {
+                animationFlow.zip(effect) { _, effect ->
+                    when (effect) {
                         RouteMainPage -> startActivity<MainActivity> { }
                         is RouteLoginPage -> startActivity<LoginActivity> { }
                     }
                     finish()
-                }
+                }.single()
             }
         }
     }
